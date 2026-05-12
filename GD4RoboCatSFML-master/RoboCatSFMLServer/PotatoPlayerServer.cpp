73d42700f85d027ba53654ac0427d435b1ea6cf4
Joseph <145919209+Swifty433@users.noreply.github.com>
2026-05-13 00:10:41 +0100

    🚧 Server side for Potato Player

--- GD4RoboCatSFML-master/RoboCatSFMLServer/PotatoPlayerServer.cpp
+++ GD4RoboCatSFML-master/RoboCatSFMLServer/PotatoPlayerServer.cpp
@@ -1,2 +1,63 @@
+// Joseph Byrne D00255161
+
 #include "RoboCatServerPCH.hpp"
-#include "PotatoPlayerServer.hpp"
+
+PotatoPlayerServer::PotatoPlayerServer() {}
+
+void PotatoPlayerServer::HandleDying()
+{
+    NetworkManagerServer::sInstance->UnregisterGameObject(this);
+}
+
+void PotatoPlayerServer::Update()
+{
+    Vector3 oldLocation = GetLocation();
+    Vector3 oldVelocity = GetVelocity();
+    float   oldRotation = GetRotation();
+    bool    oldHasPotato = HasPotato();
+
+    ClientProxyPtr client =
+        NetworkManagerServer::sInstance->GetClientProxy(GetPlayerId());
+    if (client)
+    {
+        MoveList& moveList = client->GetUnprocessedMoveList();
+        for (const Move& move : moveList)
+        {
+            ProcessInput(move.GetDeltaTime(), move.GetInputState());
+            SimulateMovement(move.GetDeltaTime());
+        }
+        moveList.Clear();
+    }
+
+    if (!RoboMath::Is2DVectorEqual(oldLocation, GetLocation()) ||
+        !RoboMath::Is2DVectorEqual(oldVelocity, GetVelocity()) ||
+        oldRotation != GetRotation())
+    {
+        NetworkManagerServer::sInstance->SetStateDirty(
+            GetNetworkId(), PotatoPlayer::EPRS_Pose);
+    }
+
+    if (oldHasPotato != HasPotato())
+    {
+        NetworkManagerServer::sInstance->SetStateDirty(
+            GetNetworkId(), PotatoPlayer::EPRS_Potato);
+    }
+}
+
+void PotatoPlayerServer::ReceivePotato()
+{
+    SetHasPotato(true);
+    NetworkManagerServer::sInstance->SetStateDirty(
+        GetNetworkId(), PotatoPlayer::EPRS_Potato);
+}
+
+void PotatoPlayerServer::ExplodePotato()
+{
+    SetHasPotato(false);
+    mHealth = 0;
+    SetDoesWantToDie(true);
+    NetworkManagerServer::sInstance->SetStateDirty(
+        GetNetworkId(), PotatoPlayer::EPRS_Health);
+    NetworkManagerServer::sInstance->SetStateDirty(
+        GetNetworkId(), PotatoPlayer::EPRS_Potato);
+}
--- GD4RoboCatSFML-master/RoboCatSFMLServer/PotatoPlayerServer.hpp
+++ GD4RoboCatSFML-master/RoboCatSFMLServer/PotatoPlayerServer.hpp
@@ -1,5 +1,24 @@
+// Joseph Byrne D00255161
+
 #pragma once
-class PotatoPlayerServer
+
+class PotatoPlayerServer : public PotatoPlayer
 {
+public:
+    static GameObjectPtr StaticCreate()
+    {
+        return NetworkManagerServer::sInstance->RegisterAndReturn(
+            new PotatoPlayerServer());
+    }
+
+    virtual void HandleDying() override;
+    virtual void Update() override;
+
+    void ReceivePotato();
+    void ExplodePotato();
+
+protected:
+    PotatoPlayerServer();
 };
 
+typedef shared_ptr<PotatoPlayerServer> PotatoPlayerServerPtr;
